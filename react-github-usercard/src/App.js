6643446bef82deea3c296dcbc5725cd40e9c5881
astubbings <53932400+astubbings@users.noreply.github.com>
2019-12-04 08:23:36 -0700

    app updated just presentational

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Profile from './components/Profile';
+import Followers from '/components/Followers';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub User Card</h1>
+      <h2>The React Lifecycle</h2>
+      <h3>Tuesday Week 9</h3>
+      <Profile/>
+      <Followers/>
+
     </div>
   );
 }
